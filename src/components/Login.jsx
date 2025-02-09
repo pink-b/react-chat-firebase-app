9213b9fc8dade5fe45f5fedc87ccbaae1692832b
pink-b <60101962+pink-b@users.noreply.github.com>
2025-02-09 20:56:04 +0400

    Add Login Window and Login, End Buttons

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,29 @@
+import { Box, Button, Container, Grid } from '@mui/material';
 import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            Login
-        </div>
+        <Container>
+            <Grid container 
+                  style={{height: window.innerHeight - 200}}
+                  alignItems={"center"}
+                  justifyContent={"center"}>
+                      <Grid
+            style={{width: 400, background: 'lightgray'}}
+            container 
+            alignItems={'center'}
+            direction={'column'}
+            >
+                <Box p={5}>
+                    <Button variant={"outlined"}>
+                        Войти с помощью Google
+                    </Button>
+                </Box>
+            </Grid>
+
+            </Grid>
+          
+        </Container>
     );
 };
-
 export default Login;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Button, Grid } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../utils/consts';
 
 const NavBar = () => {
+
+    const user = true;
+
     return (
-        <div>
-            NavBar
-        </div>
+        <AppBar position="static">
+        <Toolbar variant={"dense"}>
+            <Grid container justifyContent="flex-end">
+                {user ?
+                <Grid>
+                    <Button variant="outlined" color="inherit" sx={{marginRight: "10px"}}>
+                        Логин
+                    </Button>
+                    <Button variant="outlined" color="inherit">
+                        Регистрация
+                    </Button>
+                </Grid>
+                : 
+                <NavLink to={LOGIN_ROUTE}>
+                    <Button variant={"outlined"} color="inherit">Выйти</Button>
+                </NavLink>
+                } 
+                
+                
+            </Grid>
+        </Toolbar>
+        </AppBar>
     );
 };
 
